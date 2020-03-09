e4eec6c05aaa953cccc9c8ef9a0fa034ee57d211
QIN DING <qding@ucdavis.edu>
2020-03-09 15:31:57 -0700

    Add files via upload
    final mixed model

--- fit_linear_mixed_model.py
+++ fit_linear_mixed_model.py
@@ -1,18 +1,76 @@
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
-import pandas as pd
-import numpy as np
 
+# coding: utf-8
+
+# In[7]:
+
+
+import pandas as pd
+import sklearn.preprocessing
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from pandas.plotting import scatter_matrix
+
+# Load the data
 data = pd.read_csv("data/data_for_analysis.csv")
 
 
-linear_mixed_model_data = data.dropna(subset=["birth_year", "cave_canem_indicator"])
-# Fit model for f0Range2sd
-linear_mixed_model = smf.mixedlm("f0Range2sd ~ birth_year + cave_canem_indicator", linear_mixed_model_data,
-                                 groups=linear_mixed_model_data["poet_full_name"])
-linear_mixed_model_fit = linear_mixed_model.fit()
-print(linear_mixed_model_fit.summary())
+# In[8]:
 
 
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 
+
+# In[9]:
+
+
+data['name'] = data['Author last name'].str.strip() + data['Author first name'].str.strip()
+data['name'] = data['name'].str.lower()
+
+
+# In[10]:
+
+
+data.rename(columns={'Birth Year': 'birthyear'}, inplace=True)
+
+
+# In[11]:
+
+
+data.columns
+
+
+# In[13]:
+
+
+set(data['Region'])
+
+
+# In[14]:
+
+
+data['Region'].fillna(value = 'Other', inplace = True)
+data['Region'].replace(['Various', 'Unknown', 'British'], 'Other', inplace = True)
+set(data['Region'])
+
+
+# In[16]:
+
+
+model_dyn = smf.mixedlm("Dynamism ~ C(cave_canem_indicator) + C(graduate_study_indicator) + C(Region, Treatment('Caribbean'))", data, groups=data["name"])
+dyn = model_dyn.fit()
+print()
+print(dyn.summary())
+
+
+# In[17]:
+
+
+model_pauserate = smf.mixedlm("MeanPauseDuration ~ C(cave_canem_indicator) + C(undergrad_study_indicator) + C(Region, Treatment('Caribbean'))", data, groups=data["name"])
+pauserate = model_pauserate.fit()
+print(pauserate.summary())
 
