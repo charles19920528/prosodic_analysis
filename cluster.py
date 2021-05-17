3152d643362ae5acd1a8e765eb3d5a660f1fd97f
Charles <charles19920528@hotmail.com>
2021-05-17 12:41:18 -0700

    Update the scripts for new data.

--- cluster.py
+++ cluster.py
@@ -3,6 +3,7 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import json
 
 ################
 # Data loading #
@@ -20,7 +21,7 @@ intensity_name_vet = ['IntensitySegmentMeanSD', 'IntensityMeanAbsVelocity', 'Int
 
 def plot_cluster(measurement_name_vet, ncluster, data_frame):
     """
-    Perform K-means clustering, provide a 3d plot, and return poets names.
+    Perform K-means clustering, provide a 2d or 3d plot, and return poets names.
     :param measurement_name_vet: A vector of length 3 contains names of measurement to be plotted.
     :param ncluster: An integer which specifies the number of clusters.
     :param data_frame: A pandas data frame. The default is the data frame produced by the data_preprocessing script.
@@ -30,7 +31,7 @@ def plot_cluster(measurement_name_vet, ncluster, data_frame):
     """
     measurement_data_frame = data_frame[measurement_name_vet]
     column_names_vet = measurement_data_frame.columns
-    names_series = data_frame['Author last name'] + " " + data_frame['Author first name']
+    names_series = data_frame['author_last_name'] + " " + data_frame['author_first_name']
 
     kmeans = KMeans(n_clusters=ncluster, random_state=0).fit(measurement_data_frame)
 
@@ -40,16 +41,24 @@ def plot_cluster(measurement_name_vet, ncluster, data_frame):
 
     for i in range(ncluster):
         idx = (kmeans.labels_ == i)
-        ax.scatter3D(measurement_data_frame[column_names_vet[0]][idx], measurement_data_frame[column_names_vet[1]][idx],
-                     measurement_data_frame[column_names_vet[2]][idx], color=color_dict[i], label=i)
+        if len(measurement_name_vet) == 3:
+            ax.scatter3D(measurement_data_frame[column_names_vet[0]][idx],
+                         measurement_data_frame[column_names_vet[1]][idx],
+                         measurement_data_frame[column_names_vet[2]][idx], color=color_dict[i], label=i)
+        else:
+            ax.scatter3D(measurement_data_frame[column_names_vet[0]][idx],
+                         measurement_data_frame[column_names_vet[1]][idx], color=color_dict[i], label=i)
         ax.legend()
         ax.set_xlabel(column_names_vet[0])
         ax.set_ylabel(column_names_vet[1])
-        ax.set_zlabel(column_names_vet[2])
+        if len(measurement_name_vet) == 3:
+            ax.set_zlabel(column_names_vet[2])
     plt.show()
     poets_cluster_dictionary = {}
     for i in range(ncluster):
-        poets_cluster_dictionary[i] = names_series[(kmeans.labels_ == i)]
+        poets_cluster_dictionary[i] = pd.concat([names_series[kmeans.labels_ == i],
+                                                 data_frame.loc[kmeans.labels_ == i, "poem_title"]], axis=1)
+
     return poets_cluster_dictionary
 
 
@@ -58,11 +67,11 @@ pause_cluster_dictionary = plot_cluster(measurement_name_vet=pause_name_vet, ncl
 complexity_cluster_dictionary = plot_cluster(measurement_name_vet=complexity_name_vet, ncluster=4, data_frame=data)
 intensity_cluster_dictionary = plot_cluster(measurement_name_vet=intensity_name_vet, ncluster=2, data_frame=data)
 
-
-cluster_to_report_dictionary = plot_cluster(measurement_name_vet=['IntensityMeanAbsVelocity', "Dynamism",
+cluster_to_report_dictionary = plot_cluster(measurement_name_vet=['IntensityMeanAbsVelocity',
+                                                                  'dynamism_original_formula',
                                                                   'MeanPauseDuration'], ncluster=2, data_frame=data)
-poets_cluster_1 = cluster_to_report_dictionary[0].unique()
-poets_cluster_2 = cluster_to_report_dictionary[1].unique()
+cluster_to_report_dictionary[0].to_csv("cluster_0.csv")
+cluster_to_report_dictionary[1].to_csv("cluster_1.csv")
 
 # Poets have recordings only in the first group
 poets_cluster_1[~np.in1d(poets_cluster_1, poets_cluster_2)]
--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,37 +1,35 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("data/processed_data.csv")
-data.rename(columns = {'Cave Canem?': "cave_canem_indicator", 'Public 0 /Private 1': "public_private_indicator"},
-            inplace=True)
+data = pd.read_excel("data/101AfAmWomenPoetsVoxitResults5-3-21.xlsx", engine='openpyxl', nrows=202)
+data = data[data.columns[:26].append(data.columns[-1:]).append(data.columns[26:-1])]
+data.columns = ["_".join(x.strip().lower().split(" ")) for x in data.columns[:27]] + data.columns[27:].to_list()
 
-# Impute missing data with mean
-data['MeanPauseDuration'].fillna(data['MeanPauseDuration'].mean(), inplace=True)
+data.rename({"cave_canem_(fellow,_faculty,_or_board)": "cave_canem",
+             'public_0_/private_1': "private_school", "undergrad/comm._college": "undergraduate_school",
+             'recording:_live,_studio,_or_self-recorded': "recording",
+             'audience:academic?_spoken_word?_poetry_reading_space?_poetry_festival?_cave_canem_channel?_bar_reading_series?_book_fair?_residence?_gallery?_bookstore?_other?':
+            "audience", 'major_award_(1),_none_(0)': 'major_award'}, axis=1, inplace=True)
+data["birth_year"] = data["birth_year"].replace({0: np.nan})
+data[["spoken_word", "cave_canem"]] = data[["spoken_word", "cave_canem"]].replace({0: False, 1: True})
 
-# process region
-data['Region'].fillna('Other', inplace = True)
-data['Region'].replace(['Various', 'Unknown', 'British'], 'Other', inplace=True)
-set(data['Region'])
+# drop rows without prosodic measurements
+data = data.loc[data["f0Mean"].notna(), :]
 
-# create indicator variables for education
-undergrad_study_indicator = pd.Series(np.array([1-int(x) for x in data['Community college or university'].isnull()]))
-graduate_study_indicator = pd.Series(np.array([1-int(x) for x in data['Graduate'].isnull()]))
+data['region'] = data['region'].replace(['Puerto Rico/West', 'Unknown', 'British', 'South/West'], 'Other')
 
-# create name column
-names_series = data['Author last name'].str.strip() + " " + data['Author first name'].str.strip()
+# No school means not attending?
+# Some poets have grad schools but not undergrad instituions.
+data.loc[data['graduate_school'].notna(), 'graduate_school'] = True
+data.loc[data['graduate_school'].isna(), 'graduate_school'] = False
+data.loc[data["undergraduate_school"].notna(), "undergraduate_school"] = True
+data.loc[data["undergraduate_school"].isna(), "undergraduate_school"] = False
 
-new_data_dict = {"undergrad_study_indicator": undergrad_study_indicator,
-                 "graduate_study_indicator": graduate_study_indicator,
-                 "poet_full_name": names_series}
+# Strip out spaces in author names.
+for col in data.columns[:2]:
+    data[col] = data[col].str.strip()
+data.replace({'Alison C': 'Alison C.'}, inplace=True)
 
-data = pd.concat([data, pd.DataFrame(new_data_dict)], axis=1)
-
-# change some column names
-data = data.rename(columns={"Birth Year": "birth_year", "Region": "region"})
-
-
-
-
-data.to_csv("data/data_for_analysis.csv")
+data.to_csv("data/data_for_analysis.csv", index=False)
 
 
