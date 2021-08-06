df643fb2423b3ad2ee3402ab90974b90b540a1f5
Charles <charles19920528@hotmail.com>
2021-08-06 13:51:40 -0700

    Move some data process parts from the notebook to the script. Confirmed that the results of IntensityMeanAbsVelocity model are not significant.

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -10,7 +10,7 @@ data.rename({"cave_canem_(fellow,_faculty,_or_board)": "cave_canem",
              'public_0_/private_1': "private_school", "undergrad/comm._college": "undergraduate_school",
              'recording:_live,_studio,_or_self-recorded': "recording",
              'audience:_academic_(poetry_festivals_and_universities)?_spoken_word?_public_reading_space_(bookstores,_bars,_galleries)?':
-            "audience", 'major_award_(1),_none_(0)': 'major_award', "Poem Title": "poem_title"}, axis=1, inplace=True)
+            "cave_canem_indicator", 'major_award_(1),_none_(0)': 'major_award', "Poem Title": "poem_title"}, axis=1, inplace=True)
 
 data["birth_year"] = data["birth_year"].replace({0: np.nan})
 data[["spoken_word", "cave_canem"]] = data[["spoken_word", "cave_canem"]].replace({0: False, 1: True})
@@ -31,6 +31,18 @@ data.loc[data["undergraduate_school"].isna(), "undergraduate_school"] = False
 for col in data.columns[:2]:
     data[col] = data[col].str.strip()
 data.replace({'Alison C': 'Alison C.'}, inplace=True)
+
+data = data.rename(columns={
+    'Dynamism Original Formula': "Dynamism",
+    'private_school': "public_private_indicator",
+    'undergraduate_school': 'undergrad_study_indicator',
+    'graduate_school': 'graduate_study_indicator',
+    'ivy': 'Ivy',
+    'cave_canem': 'cave_canem_indicator',
+})
+
+# make author names in the format (last_name first_name)
+data['poet_full_name'] = data['author_last_name'] + ' ' + data['author_first_name']
 
 data.to_csv("data/data_for_analysis.csv", index=False)
 
